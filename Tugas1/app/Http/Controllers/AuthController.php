844cfe1ca5b792e04064b52ae813243ba4da9c5c
Michael Hermanto <michhu1212@gmail.com>
2025-10-07 16:00:31 +0700

    hehe

--- Tugas1/app/Http/Controllers/AuthController.php
+++ Tugas1/app/Http/Controllers/AuthController.php
@@ -6,5 +6,77 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        try{
+            $validated = $request->safe()->all();
+            $passwordHash = Hash::make($validated ['password']);
+            $validated ['password'] = $passwordHash;
+            $response = User::create($validated);
+
+            if($response){
+                return response() ->json([
+                    'message' => 'Register Success',
+                    'user' => $response
+                ],201);
+            }
+
+        }catch (Exception $e){
+            return response()->json([
+                'message' => $e ->getMessage(),
+                'data' => null
+            ],500);
+        }
+    }
+
+    public function login (LoginRequest $request)
+    {
+        try{
+            $validated = $request->safe()->all();
+
+            if(!Auth::attempt($validated)){
+                return response()->json([
+                    'message',
+                    'data' => null ],401);
+            }
+
+            $user = $request->user();
+            $token = $user->createToken('laravel_api',['*'])->plainTextToken;
+
+            return response()->json([
+                'message' => 'Login Success',
+                'user' => $user,
+                'token' => $token
+            ],200);
+
+
+
+
+
+
+        }catch (Exception $e){
+            return response()->json([
+                'message' => $e ->getMessage(),
+                'data' => null
+            ],500);
+        }
+    }
+
+    public function logout(Request $request){
+        try{
+            $request->user()->currentAccessToken()->delete();
+
+            return response()->json([
+                'messagge' => 'Logout Success',
+                'data' => null
+            ],200);
+
+        }catch(Exception $e){
+            return response()->json([
+                'messagge' => $e->getMessage(),
+                'data' => null
+            ],500);
+
+        }
+    }
 }
